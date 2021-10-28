d51952be0c9538f6550f25771e71adb043909edc
yunyaomingzheng <35861629+yunyaomingzheng@users.noreply.github.com>
2021-10-28 20:05:28 +0800

    code check

--- hummingbot/connector/exchange/mexc/mexc_order_book.pyx
+++ hummingbot/connector/exchange/mexc/mexc_order_book.pyx
@@ -79,74 +79,23 @@ cdef class MexcOrderBook(OrderBook):
 
     @classmethod
     def snapshot_message_from_db(cls, record: RowProxy, metadata: Optional[Dict] = None) -> OrderBookMessage:
-        ts = record["timestamp"]
-        msg = record["json"] if type(record["json"]) == dict else ujson.loads(record["json"])
-        if metadata:
-            msg.update(metadata)
-
-        return MexcOrderBookMessage(OrderBookMessageType.SNAPSHOT, {
-            "trading_pair": msg["ch"].split(".")[1],
-            "update_id": int(ts),
-            "bids": msg["tick"]["bids"],
-            "asks": msg["tick"]["asks"]
-        }, timestamp=ts * 1e-3)
+        pass
 
     @classmethod
     def diff_message_from_db(cls, record: RowProxy, metadata: Optional[Dict] = None) -> OrderBookMessage:
-        ts = record["timestamp"]
-        msg = record["json"] if type(record["json"]) == dict else ujson.loads(record["json"])
-        if metadata:
-            msg.update(metadata)
-        return MexcOrderBookMessage(OrderBookMessageType.DIFF, {
-            "trading_pair": msg["s"],
-            "update_id": int(ts),
-            "bids": msg["b"],
-            "asks": msg["a"]
-        }, timestamp=ts * 1e-3)
+        pass
 
     @classmethod
     def snapshot_message_from_kafka(cls, record: ConsumerRecord, metadata: Optional[Dict] = None) -> OrderBookMessage:
-        ts = record.timestamp
-        msg = ujson.loads(record.value.decode())
-        if metadata:
-            msg.update(metadata)
-        return MexcOrderBookMessage(OrderBookMessageType.SNAPSHOT, {
-            "trading_pair": msg["ch"].split(".")[1],
-            "update_id": ts,
-            "bids": msg["tick"]["bids"],
-            "asks": msg["tick"]["asks"]
-        }, timestamp=ts * 1e-3)
+        pass
 
     @classmethod
     def diff_message_from_kafka(cls, record: ConsumerRecord, metadata: Optional[Dict] = None) -> OrderBookMessage:
-        decompressed = bz2.decompress(record.value)
-        msg = ujson.loads(decompressed)
-        ts = record.timestamp
-        if metadata:
-            msg.update(metadata)
-        return MexcOrderBookMessage(OrderBookMessageType.DIFF, {
-            "trading_pair": msg["s"],
-            "update_id": ts,
-            "bids": msg["bids"],
-            "asks": msg["asks"]
-        }, timestamp=ts * 1e-3)
+        pass
 
     @classmethod
     def trade_message_from_db(cls, record: RowProxy, metadata: Optional[Dict] = None):
-        msg = record["json"]
-        ts = record.timestamp
-        data = msg["tick"]["data"][0]
-        if metadata:
-            msg.update(metadata)
-        return MexcOrderBookMessage(OrderBookMessageType.TRADE, {
-            "trading_pair": msg["ch"].split(".")[1],
-            "trade_type": float(TradeType.BUY.value) if data["direction"] == "sell"
-            else float(TradeType.SELL.value),
-            "trade_id": ts,
-            "update_id": ts,
-            "price": data["price"],
-            "amount": data["amount"]
-        }, timestamp=ts * 1e-3)
+        pass
 
     @classmethod
     def from_snapshot(cls, msg: OrderBookMessage) -> OrderBook:
